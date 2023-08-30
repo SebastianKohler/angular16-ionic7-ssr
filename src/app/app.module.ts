c4c1225ffa81a6670158b204de3c83ec43ba3661
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2023-08-30 06:37:32 +0300

    Create Angular 16 app with SSR enabled and add Ionic 7

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicModule } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(
+      {
+        mode: 'md',
+      }
+    ),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
